8f61beadf4b8a3f18ec7f12fac8c3a0345c1a742
JoelMac <jr92work@gmail.com>
2022-06-28 18:19:00 +0200

    01

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
+function suma(){
+    // let num1 = parseInt(document.getElementById("n1").value);
+    // let num2 = parseInt(document.getElementById("n2").value);
+    // let resu = num1+num2;
+    // document.getElementById("re").innerHTML = resu;
 
+    // one num calcutator
+
+    let dd = document.getElementById("n1").value+="+";
+
+    
+}
+function resta(){
+    // let num1 = parseInt(document.getElementById("n1").value);
+    // let num2 = parseInt(document.getElementById("n2").value);
+    // let resu = num1-num2;
+    // document.getElementById("re").innerHTML = resu;
+
+    // one num calcutator
+
+    let dd = document.getElementById("n1").value+="-";
+}
+function multi(){
+    // let num1 = parseInt(document.getElementById("n1").value);
+    // let num2 = parseInt(document.getElementById("n2").value);
+    // let resu = num1*num2;
+    // document.getElementById("re").innerHTML = resu;
+
+    // one num calcutator
+
+    let dd = document.getElementById("n1").value+="*";
+}
+function divi(){
+    // let num1 = parseInt(document.getElementById("n1").value);
+    // let num2 = parseInt(document.getElementById("n2").value);
+    // let resu = num1/num2;
+    // document.getElementById("re").innerHTML = resu;
+
+    // one num calcutator
+
+    let dd = document.getElementById("n1").value+="/";
+}
+function igual(){
+    //try to split one by one numbers
+    
+    let resu = Number(document.getElementById("n1").value);
+    // let x = resu.valueOf();
+    // alert(resu);
+    document.getElementById("re").innerHTML = resu;
+    
+    
+}
+function one(){
+    let dd = document.getElementById("n1").value+="1";
+    // alert(dd);
+}
+function two(){
+    let dd = document.getElementById("n1").value+="2";
+    // alert(dd);
+}
+function three(){
+    let dd = document.getElementById("n1").value+="3";
+    // alert(dd);
+} 
+function four(){
+    let dd = document.getElementById("n1").value+="4";
+    // alert(dd);
+}
+function five(){
+    let dd = document.getElementById("n1").value+="5";
+    // alert(dd);
+}
+function six(){
+    let dd = document.getElementById("n1").value+="6";
+    // alert(dd);
+}
+function seven(){
+    let dd = document.getElementById("n1").value+="7";
+    // alert(dd);
+}
+function eight(){
+    let dd = document.getElementById("n1").value+="8";
+    // alert(dd);
+}
+function nine(){
+    let dd = document.getElementById("n1").value+="9";
+    // alert(dd);
+}
+function cero(){
+    let dd = document.getElementById("n1").value+="0";
+    // alert(dd);
+}
